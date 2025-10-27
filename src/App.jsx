b2f203b97e14a75c8dbf31b36bf110b1abbd39a2
Daybreak <bsrohith2002@gmail.com>
2025-10-27 17:38:27 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import DMIStart from "./pages/DMIStart.jsx";
 import DMIWizard from "./pages/DMIWizard.jsx";
 import DMIResult from "./pages/DMIResult.jsx";
 import DMIResultPreview from "./pages/DMIResultPreview.jsx";
 import { useIframeHeight } from "./hooks/useIframeHeight.js";
-
 function HomePage() {
   useIframeHeight();
   
@@ -42,19 +42,22 @@ function HomePage() {
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/dmi/start" element={<DMIStart />} />
-      <Route path="/dmi/org" element={<DMIWizard step="org" />} />
-      <Route path="/dmi/vision" element={<DMIWizard step="vision" />} />
-      <Route path="/dmi/leadership" element={<DMIWizard step="leadership" />} />
-      <Route path="/dmi/people" element={<DMIWizard step="people" />} />
-      <Route path="/dmi/research" element={<DMIWizard step="research" />} />
-      <Route path="/dmi/ops" element={<DMIWizard step="ops" />} />
-      <Route path="/dmi/innovation" element={<DMIWizard step="innovation" />} />
-      <Route path="/dmi/result" element={<DMIResult />} />
-      <Route path="/preview-results" element={<DMIResultPreview />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dmi/start" element={<DMIStart />} />
+        <Route path="/dmi/org" element={<DMIWizard step="org" />} />
+        <Route path="/dmi/vision" element={<DMIWizard step="vision" />} />
+        <Route path="/dmi/leadership" element={<DMIWizard step="leadership" />} />
+        <Route path="/dmi/people" element={<DMIWizard step="people" />} />
+        <Route path="/dmi/research" element={<DMIWizard step="research" />} />
+        <Route path="/dmi/ops" element={<DMIWizard step="ops" />} />
+        <Route path="/dmi/innovation" element={<DMIWizard step="innovation" />} />
+        <Route path="/dmi/result" element={<DMIResult />} />
+        <Route path="/preview-results" element={<DMIResultPreview />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
